525d39017b62fc1a999aa4df9d076d7145674860
plumcro <serenityhafeez@gmail.com>
2024-06-06 17:16:56 +0530

    Name and Bg Color

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
+  const name = 'Mohammed Hafeez';
   greetUser(name);
+  document.body.style.backgroundColor = "plum";
 });
